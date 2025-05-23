47acc9f99503a034cd79f5bc3c99925b48225da6
Gaurav Srivastav <gauravsri271@gmail.com>
2025-05-23 11:35:29 +0530

    Update practical2.cpp

--- practical2.cpp
+++ practical2.cpp
@@ -1,51 +1,20 @@
-// remove dublicates from an array ...
-
-#include<iostream>
+#include <iostream>
+#include <set>
 using namespace std;
-int removedublicates(int arr[],int n){
-    if(n==0 || n==1){
-        return n;
+
+int main() {
+    int arr[] = {4, 2, 4, 3, 2, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    set<int> s;  // Set automatically removes duplicates
+    for (int i = 0; i < n; i++) {
+        s.insert(arr[i]);
     }
-    int temp[n];   //temperory array
-    int j= 0 ;          //index of temperory arry
 
-    for(int i =0;i<n-1;i++){
-        if(arr[i] !=arr[i+1]){        //if adject index not equal to then 
-            temp[j++]=arr[i];       // arry of index i is store in temperory arry and increment by 1
-        }
-       
+    cout << "Array after removing duplicates: ";
+    for (int x : s) {
+        cout << x << " ";
     }
-    temp[j++] = arr[n-1];    // store thelast element 
-
-    //now we back to the store in original array
-    for(int i=1;i<j;i++){
-        arr[i] = temp[i];
-    }
-    return j;
-}
-
-int main(){
-    int n;
-    cout<<"enter the no of elements : ";   //number the total  elements
-    cin>>n;
-
-    int arr[n];
-    cout<<"enter the elements :";
-    for(int i =0;i<n;i++){ // number the elements
-        
-        cin>>arr[i];
-    }
-    //now calling the function and stroe the array in new size variable
-
-    int newsize = removedublicates(arr,n);
-
-    //print the new size array
-    cout<<"the new array is -->";
-    for(int i =0;i<newsize;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-
+    cout << endl;
     return 0;
-
 }
